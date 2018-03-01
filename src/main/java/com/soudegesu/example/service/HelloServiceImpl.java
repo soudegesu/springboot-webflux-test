3801ecab02e086855be90f241e554b8d2b7fe00d
soudegesu <soudegesu@hotmail.co.jp>
2018-03-01 12:20:55 +0900

    modify service

--- src/main/java/com/soudegesu/example/service/HelloServiceImpl.java
+++ src/main/java/com/soudegesu/example/service/HelloServiceImpl.java
@@ -1,13 +1,9 @@
 package com.soudegesu.example.service;
 
-import java.util.Random;
-
 public class HelloServiceImpl implements HelloService {
 
-    private static Random RANDOM = new Random();
-
     @Override
-    public String getCodeStr() {
-        return String.valueOf(RANDOM.nextInt());
+    public Thread getCurrentThread() {
+        return Thread.currentThread();
     }
 }
