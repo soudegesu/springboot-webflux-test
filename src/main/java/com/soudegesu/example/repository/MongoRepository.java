51f91c0079a6eeb3c1b212e47ce506b9435a6879
soudegesu <soudegesu@hotmail.co.jp>
2018-03-07 14:41:36 +0900

    add mongo driver

--- src/main/java/com/soudegesu/example/repository/MongoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.soudegesu.example.repository;
-
-import com.soudegesu.example.data.User;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-
-public interface MongoRepository extends ReactiveCrudRepository<User, String> {
-
-}
