f604870130f2c7b0533f2c23ae73898a7c98b47d
soudegesu <soudegesu@hotmail.co.jp>
2018-03-13 16:10:19 +0900

    add auto configure

--- modules/webflux/src/main/java/com/soudegesu/example/webflux/HelloWebFluxApplication.java
+++ modules/webflux/src/main/java/com/soudegesu/example/webflux/HelloWebFluxApplication.java
@@ -1,10 +1,12 @@
 package com.soudegesu.example.webflux;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class HelloWebFluxApplication {
 
     public static void main(String[] args) {
